8f01eb230c682546f180be6bcdfe985c02c9574e
Patrick <Patrick.Hwang@infinite.com>
2022-10-08 11:45:35 -0500

    bot skeleton

--- README.txt
+++ README.txt
@@ -1,1 +1,10 @@
-hi
+Language minigame bot for discord, similar to mudae's tea games
+
+Functions:
+
+ - Type a word in a language and give translations from a dictionary database (wiktitonary, jisho) 
+ - supported languages (japanese, chinese)
+
+ For Devs:
+ Run with "node bot.js"
+ 
---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
+
+// Configure logger settings
+
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+  colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+
+const bot = new Discord.Client({
+  token: auth.token,
+  autorun: true
+});
+
+bot.on('ready', function (evt) {
+  logger.info('Connected');
+  logger.info('Logged in as: ');
+  logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+
+
+let users = {};
+
+function sendMessage(channelID, message) {
+  bot.sendMessage({
+    to: channelID,
+    message
+  });
+}
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+  if (message.substring(0,1) === '!') {
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
+
+    args = args.splice(1);
+    switch(cmd) {
+      case 'ping':
+        bot.sendMessage(channelID, 'Pong!');
+        break;
+    }
+    console.log(message);
+  }
+});
